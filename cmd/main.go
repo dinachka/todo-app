5334cf5b8e57766cb33f70959ad2e2afdc3d0232
dina <dinachernykh1@gmail.com>
2023-01-24 15:41:30 +0400

    db init

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,47 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/dinachka/todo-app"
 	"github.com/dinachka/todo-app/pkg/handler"
+	"github.com/dinachka/todo-app/pkg/repository"
+	"github.com/dinachka/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("initConfig", err.Error())
+	}
+
+		if err:= godotenv.Load(); err != nil {
+			log.Fatalf("dotenv.Load", err.Error())
+		}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username:  viper.GetString("db.username"),
+		Password:  os.Getenv("DB_PASSWORD"),
+		DBName:    viper.GetString("db.dbname"),
+		SSLMode:   viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("dbErr", err.Error())
+	}
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatal(err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
